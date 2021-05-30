9e17f85b9b4398bee1aa9dba2178c47c10b46904
SangchoKim <wjdrms1919@gmail.com>
2021-05-31 00:29:31 +0900

    feat: add nav

--- src/components/AppLayout/AppLayout.jsx
+++ src/components/AppLayout/AppLayout.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import { css } from '@emotion/react';
 import 'normalize.css';
+import { Link } from '@reach/router';
+import BackGroundImg from '../../assets/images/cool-background.png';
+import HomeIcon from '../../assets/images/c6730f_331e4a84182944a9a7a892945436f3de_mv2.webp';
+import AboutIcon from '../../assets/images/c6730f_0ae73585c16049f789c3d462512a84bf_mv2.webp';
+import ContactIcon from '../../assets/images/c6730f_81f62b0326834095a11e062e1638d790_mv2.webp';
+import PortfolioIcon from '../../assets/images/gotoMain.svg';
 
 export default function AppLayout({ children }) {
   return <div css={globalStyle}>{children}</div>;
 }
 
 function Side({ children }) {
-  return <aside css={sidebarStyle}>{children}</aside>;
+  return (
+    <aside css={sidebarStyle}>
+      <div>
+        <Icon herf="/" src={HomeIcon} alt="home" />
+      </div>
+      <ul css={navStyle}>
+        <li>
+          <Icon herf="/" src={HomeIcon} alt="home" />
+        </li>
+        <li>
+          <Icon herf="/about" src={AboutIcon} alt="about" />
+        </li>
+        <li>
+          <Icon herf="/contact" src={ContactIcon} alt="contact" />
+        </li>
+      </ul>
+      <div>
+        <Icon herf="/" src={PortfolioIcon} alt="portfolio" />
+      </div>
+    </aside>
+  );
 }
 
 function Main({ children }) {
@@ -18,17 +44,41 @@ AppLayout.Main = Main;
 
 const globalStyle = css`
   font-family: 'barlow';
+  background: url(${BackGroundImg}) no-repeat;
+  background-size: cover;
+  height: 100vh;
 `;
 
 const sidebarStyle = css`
-  width: 16.25rem;
   height: 100%;
   position: fixed;
+  margin: 0 60px;
   display: flex;
-  padding-top: 3rem;
-  padding-bottom: 3rem;
-  padding-left: 3rem;
+  flex-direction: column;
+  justify-content: space-around;
+  background: transparents;
 `;
+
+const navStyle = css`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  li {
+    margin: 15px 0;
+  }
+`;
+
+const Icon = (props) => (
+  <Link to={props.herf}>
+    <img
+      {...props}
+      css={{
+        width: '2.2rem',
+        height: '2.2rem',
+      }}
+    />
+  </Link>
+);
 
 const mainStyle = css`
   padding-left: 2rem;
